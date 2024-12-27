14a74722c004d1963e0f18d74b2b387755eb965f
Joel Carter <joel@cartertech.net>
2024-12-27 01:48:03 -0500

    Solution for Day 7 in Python

---
+++ day07/part2.py
@@ -0,0 +1,25 @@
+import itertools
+
+total_calibration_result = 0
+with open('sample.txt') as file:
+    operators = '+', '*', '||'
+    for line in file:
+        line = line.rstrip()
+        result, operands = line.split(': ')
+        result = int(result)
+        operands = [int(operand) for operand in operands.split(' ')]
+        operation_combos = list(itertools.product(operators, repeat = len(operands) - 1))
+        for i in range(len(operation_combos)):
+            operations = operation_combos[i]
+            test = operands[0]
+            for j in range(len(operands) - 1):
+                if operations[j] == '+':
+                    test += operands[j + 1]
+                elif operations[j] == '*':
+                    test *= operands[j + 1]
+                else:
+                    test = int(f"{test}{operands[j + 1]}")
+            if test == result:
+                total_calibration_result += result
+                break
+    print(total_calibration_result)
